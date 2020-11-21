be9645eb58224efb64f2352526cb520c40754e30
Sailu <vermasailesh271@gmail.com>
2020-11-21 16:55:08 +0530

    readme changed

--- lib/providerr/auth.dart
+++ lib/providerr/auth.dart
@@ -32,7 +32,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDn--EMVtpWWcUSDVLhEsQeYk2X3DHsxoM';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=[auth api key]';
     //this both are genrated by firebase
 
     try {
